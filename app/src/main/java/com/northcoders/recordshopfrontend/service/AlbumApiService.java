4e99c114fa9d061e865c8720bad04a1e769aad34
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2024-12-19 15:21:29 +0000

    feat: add addAlbum POST method to api service

--- app/src/main/java/com/northcoders/recordshopfrontend/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshopfrontend/service/AlbumApiService.java
@@ -5,10 +5,15 @@ import com.northcoders.recordshopfrontend.model.Album;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumApiService {
 
     @GET("album")
     public Call<List<Album>> getAllAlbums();
+
+    @POST("album")
+    public Call<Album> addAlbum(@Body Album album);
 }
