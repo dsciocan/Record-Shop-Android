121e6875677e15032a11bb69a05cd679baf058b3
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2024-12-19 15:55:17 +0000

    feat: add PUT and DELETE methods to ApiService

--- app/src/main/java/com/northcoders/recordshopfrontend/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshopfrontend/service/AlbumApiService.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface AlbumApiService {
 
@@ -16,4 +19,10 @@ public interface AlbumApiService {
 
     @POST("album")
     public Call<Album> addAlbum(@Body Album album);
+
+    @PUT("album/{id}")
+    public Call<Album> updateAlbum(@Path("id") Long id, @Body Album album);
+
+    @DELETE("album/{id}")
+    public Call<String> deleteAlbum(@Path("id") Long id);
 }
