695c5709fa5185dbb7a73cfb42e5ee2f73731b2e
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2024-12-29 14:12:44 +0000

    feat: add confirmation dialog to Delete button and genreSetValidation for Update button

--- app/src/main/java/com/northcoders/recordshopfrontend/ui/updatealbum/UpdateAlbumClickHandler.java
+++ app/src/main/java/com/northcoders/recordshopfrontend/ui/updatealbum/UpdateAlbumClickHandler.java
@@ -1,14 +1,20 @@
 package com.northcoders.recordshopfrontend.ui.updatealbum;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
 
+import com.northcoders.recordshopfrontend.R;
 import com.northcoders.recordshopfrontend.model.Album;
 import com.northcoders.recordshopfrontend.ui.mainactivity.MainActivity;
 import com.northcoders.recordshopfrontend.ui.mainactivity.MainActivityViewModel;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class UpdateAlbumClickHandler {
@@ -28,7 +34,7 @@ public class UpdateAlbumClickHandler {
     public void updateAlbum(View view) {
         Album newAlbum = new Album(album.getId(), album.getName(), album.getReleaseYear(), album.getStock(), album.getImage(), album.getGenreSet(), album.getAlbumArtist());
 
-        if(Objects.equals(newAlbum.getName(), "") || Objects.equals(newAlbum.getAlbumArtist().getName(), "")) {
+        if(Objects.equals(newAlbum.getName(), "") || Objects.equals(newAlbum.getAlbumArtist().getName(), "") || Arrays.equals(newAlbum.getGenreSet(), new String[0])) {
             Toast.makeText(context, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
         } else {
             Intent intent = new Intent(context, MainActivity.class);
@@ -39,10 +45,23 @@ public class UpdateAlbumClickHandler {
     }
 
     public void deleteAlbum(View view) {
-        Intent intent = new Intent(context, MainActivity.class);
-        albumId = album.getId();
-        viewModel.deleteAlbum(albumId);
-        context.startActivity(intent);
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage("Are you sure you want to permanently delete this album?")
+                .setTitle("Delete");
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Intent intent = new Intent(context, MainActivity.class);
+                albumId = album.getId();
+                viewModel.deleteAlbum(albumId);
+                context.startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
 
 
     }
